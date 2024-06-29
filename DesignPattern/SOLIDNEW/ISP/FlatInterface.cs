d8b248357f93a2e67759828217a2f8ec7f9606d5
rahajustone <kh.rahmatullo@gmail.com>
2024-06-29 09:29:24 -0400

    Add Interface Segregation Principle

---
+++ DesignPattern/SOLIDNEW/ISP/FlatInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.SOLIDNEW.ISP
+{
+    internal interface FlatInterfaceForValidating
+    {
+        public abstract bool ValidatePaymentInfo();
+        public abstract bool ValidateShippingAddress();
+        public abstract void ProcessOrder();
+        public abstract void ProcessPaymentInfo();
+    }
+
+    public class OnlineOrder : FlatInterfaceForValidating
+    {
+        void FlatInterfaceForValidating.ProcessOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        void FlatInterfaceForValidating.ProcessPaymentInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool FlatInterfaceForValidating.ValidatePaymentInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool FlatInterfaceForValidating.ValidateShippingAddress()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
